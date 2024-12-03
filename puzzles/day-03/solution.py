7bcd46537db2074e6711e13e5f93f0d70f8812bf
Matthew Haahr <matthew.haahr@gmail.com>
2024-12-02 22:41:56 -0800

    [Day-03] Solved Day 03, Part 2 Took a bit of time to figure out, but relatively simple regex still, once I figured out re again

--- puzzles/day-03/solution.py
+++ puzzles/day-03/solution.py
@@ -1,20 +1,54 @@
 import sys
 import re
 
-pattern = re.compile(r"mul\((\d*),(\d*)\)")
+# Find just the multiplication instructions
+part1Pattern = re.compile(r"mul\((\d*),(\d*)\)")
+# Find the multiplication and control instructions
+part2Pattern = re.compile(r"mul\((\d*),(\d*)\)|do\(\)|don't\(\)")
 
 def puzzle(filename, part2):
-    ops = []
+    instructions = []
     with open(filename, 'r') as fp:
         while True:
             line = fp.readline()
             if line == '':
                 break   
+            
+            if part2 == False:
+                # Add the new instructions to the list
+                instructions.extend(part1Pattern.findall(line))
+            else:
+                # Loop over all the found instructions
+                for inst in part2Pattern.finditer(line):
+                    if inst.group(0) == "do()":
+                        # "do()" enables the multiplication
+                        instructions.append(True)
+                    elif inst.group(0) == "don't()":
+                        # "don't()" disables the multiplication
+                        instructions.append(False)
+                    else:
+                        # else just store the standard multiplication operation
+                        instructions.append([inst.group(1), inst.group(2)])           
+    
+    # If part 2, filter out the disabled instructions
+    if part2:
+        # Start enabled
+        enable = True
+        filteredInstructions = []
+        # Loop over all instructions
+        for inst in instructions:
+            if type(inst) == bool:
+                # If the instruction is a control type, update the enable flag accordingly
+                enable = inst
+            elif enable:
+                # If the instruction is not a control inst (must be multiplication) AND they are enabled, add to the new list
+                filteredInstructions.append(inst)
                 
-            ops.extend(pattern.findall(line))
-                
+        instructions = filteredInstructions
+    
     score = 0
-    for x, y in ops:
+    # Perform the multiplication instructions and sum
+    for x, y in instructions:
         score += int(x) * int(y)
         
     print(score)
