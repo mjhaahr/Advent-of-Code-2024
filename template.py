a70b4d53b6aff0380aa009bf40af8a3f45744972
Matthew Haahr <matthew.haahr@gmail.com>
2024-12-03 15:10:48 -0800

    [Day-03] Clean Up Template a bit

--- template.py
+++ template.py
@@ -1,16 +1,25 @@
 import sys
 
 def puzzle(filename, part2):
+    # Zero the Accumulator
+    score = 0
+    
+    # Open File
     with open(filename, 'r') as fp:
         while True:
+            # Loop over all lines
             line = fp.readline()
+            
+            # EOF check
             if line == '':
                 break   
-        
-    score = 0
+    
+    # Return Accumulator    
     print(score)
     
 if __name__ == "__main__":
+    # Check number of Arguments, expect 2 (after script itself)
+    # Args: input file, part number
     if (len(sys.argv) != 3):
         print(f"Error: Expected 2 arguments, got {len(sys.argv) - 1}")
         
