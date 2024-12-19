2daf87343c0eb1e6336f4ed37a4e090f5a71a56a
Matthew Haahr <matthew.haahr@gmail.com>
2024-12-18 22:16:22 -0800

    [Day-17] DAY 17 PART 2 SOLVED PROGRAMATICALLY That feels so good

--- puzzles/day-17/solution.py
+++ puzzles/day-17/solution.py
@@ -13,6 +13,7 @@ def puzzle(filename, part2):
     score = 0
     
     cpu = Computer()
+    ops = []
     
     # Open File
     with open(filename, 'r') as fp:
@@ -24,13 +25,37 @@ def puzzle(filename, part2):
             if "Register" in line:
                 cpu.addReg(splits[1][0], int(splits[2]))
             elif "Program" in line:
-                args = splits[1].split(',')
-                for opcode, operand in zip(args[::2], args[1::2]):
+                ops = splits[1].split(',')
+                for opcode, operand in zip(ops[::2], ops[1::2]):
                     cpu.addOp(int(opcode), int(operand))
     if debug:
         print(cpu)
-        
-    score = cpu.runProg()
+    
+    if not part2:
+        out = cpu.runProg()
+        score = ','.join(out)
+    else:
+        # Algo:
+        #  start with A of  + i (until 8), if the first character of the output is the next in the list, store and proceed, effectively DFS
+        toTest = list(range(7, -1, -1))
+        # Start lowest then work up
+        while True:
+            # Try a new one
+            testing = toTest.pop()
+            # Reset with A being the value to test
+            cpu.reset(testing)
+            # Run cycle
+            out = cpu.runProg()
+            # Check if at output
+            if out == ops:
+                score = testing
+                break
+            # Else: check if the current amount is equal, if so, test the next numbers
+            sub = ops[-len(out):]
+            if out == sub:
+                newBase = testing * 8
+                newList = list(range(newBase + 7, newBase -1, -1))
+                toTest.extend(newList)
     
     # Return Accumulator    
     print(score)
@@ -55,13 +80,19 @@ class Computer:
     def addOp(self, opcode, operand):
         self.prog.append((opcode, operand))
         
+    def reset(self, A=0):
+        self.A = A
+        self.B = 0
+        self.C = 0
+        self.output = []
+        
     def runProg(self):
         # Loop until PC overruns length
         pc = 0
         while pc < len(self.prog):
            pc = self.executeInst(pc)
            
-        return ','.join(self.output)
+        return self.output
            
     # Returns the updated PC
     def executeInst(self, pc):
@@ -115,14 +146,14 @@ class Computer:
                 div = 2 ** self.getCombo(operand)
                 out = self.A // div
                 if debug:
-                    print(f"  bdv: {self.A} / {div} = {out}")
+                    print(f"  bdv: A({self.A}) / c({operand})({div}) = {out}")
                 self.B = out
             
             case 7:
                 div = 2 ** self.getCombo(operand)
                 out = self.A // div
                 if debug:
-                    print(f"  cdv: {self.A} / {div} = {out}")
+                    print(f"  cdv: A({self.A}) / c({operand})({div}) = {out}")
                 self.C = out
                 
         if debug:
