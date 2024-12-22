fd0e0e0b08349adf38a5e633a2cb610036da47ed
Matthew Haahr <matthew.haahr@gmail.com>
2024-12-22 09:28:18 -0800

    [Day-22] Solved Part 2 That was actually really fun

--- puzzles/day-22/solution.py
+++ puzzles/day-22/solution.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from collections import deque, defaultdict
 
 # Modifying Path to include Repo Directory (for util import)
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
@@ -19,22 +20,73 @@ def puzzle(filename, part2):
             line = line.strip()
             numbers.append(int(line))
 
-    for number in numbers:
-        score += calcSecret(number, 2000)
+    if not part2:
+        for num in numbers:
+            score += runSecrets(num, 2000)
+    else:
+        score = findBestSequence(numbers, 2000)
+
 
     # Return Accumulator
     print(score)
 
-def calcSecret(num, iters):
+
+def runSecrets(num, iters):
     for i in range(iters):
-        num = mix(num * 64, num)
-        num = prune(num)
+        num = calcSecret(num)
 
-        num = mix(num // 32, num)
-        num = prune(num)
+    return num
 
-        num = mix(num * 2048, num)
-        num = prune(num)
+
+def findBestSequence(numbers, iters):
+    results = []
+    sequenceScores = defaultdict(lambda: 0)
+    for num in numbers:
+        results = findPrices(num, iters)
+
+        for sequence, score in findSequenceScores(results).items():
+            sequenceScores[sequence] += score
+
+    return max(sequenceScores.values())
+
+
+def findSequenceScores(prices):
+    sequenceScores = defaultdict(lambda: 0)
+    for p, s in prices:
+        if s not in sequenceScores:
+            sequenceScores[s] = p
+
+    return sequenceScores
+
+
+def findPrices(num, iters):
+    price = num % 10
+
+    results = []
+    sequence = deque()
+
+    for i in range(iters):
+        num = calcSecret(num)
+        newPrice = num % 10
+        delta = newPrice - price
+        price = newPrice
+        sequence.append(delta)
+        if len(sequence) == 4:
+            results.append((price, tuple(sequence)))
+            sequence.popleft()
+
+    return results
+
+
+def calcSecret(num):
+    num = mix(num * 64, num)
+    num = prune(num)
+
+    num = mix(num // 32, num)
+    num = prune(num)
+
+    num = mix(num * 2048, num)
+    num = prune(num)
 
     return num
 
