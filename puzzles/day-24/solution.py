01890a8062716ca60bd1a0893d74bb4c69fa6b57
Matthew Haahr <matthew.haahr@gmail.com>
2024-12-23 22:55:45 -0800

    [Day-24] Solved Part 1

---
+++ puzzles/day-24/solution.py
@@ -0,0 +1,105 @@
+import sys
+import os
+import re
+
+# Modifying Path to include Repo Directory (for util import)
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
+# Import Utilities
+import utils
+
+opPattern = re.compile(r"(.{3}) (AND|OR|XOR) (.{3}) -> (.{3})")
+
+def puzzle(filename, part2):
+    # Zero the Accumulator
+    score = 0
+
+    values = {}
+
+    ops = []
+
+    # Open File
+    with open(filename, 'r') as fp:
+        # Loop over all lines
+        # Parse starting values
+        parseVals = True
+        for line in fp.readlines():
+            line = line.strip()
+            if line == "":
+                parseVals = False
+                continue
+
+            if parseVals:
+                var = line[0:3]
+                values[var] = int(line[5])
+            else:
+                m = opPattern.match(line)
+                if m:
+                    ops.append(Op(m.group(1), m.group(3), m.group(4), m.group(2)))
+
+    # Run all operations
+    idx = 0
+    while ops:
+        op = ops[idx]
+        if op.a in values and op.b in values:
+            values[op.out] = op.run(values[op.a], values[op.b])
+            del ops[idx]
+
+            if idx >= len(ops):
+                idx = len(ops) - 1
+        else:
+            idx = (idx + 1) % len(ops)
+
+    # Reverse alphabetically sort the output value keys
+    outs = [val for val in values.keys() if val[0] == 'z']
+    outs.sort(reverse=True)
+
+    # Compress them into a string
+    bin = ''.join([str(values[out]) for out in outs])
+    # Convert binary to decimal
+    score = int(bin, 2)
+
+    # Return Accumulator
+    print(score)
+
+
+class Op:
+    def __init__(self, a, b, out, op):
+        self.a = a
+        self.b = b
+        self.out = out
+        self.op = op
+        self.ran = False
+
+    def run(self, a, b):
+        self.ran = True
+        match self.op:
+            case 'AND':
+                return a & b
+            case 'OR':
+                return a | b
+            case 'XOR':
+                return a ^ b
+
+    def __str__(self):
+        op = 'bad'
+        match self.op:
+            case 'AND':
+                op = '&'
+            case 'OR':
+                op = '|'
+            case 'XOR':
+                op = '^'
+
+        return f"op: {self.a} {op} {self.b} = {self.out}, ran = {self.ran}"
+
+    def __repr__(self):
+        return str(self)
+
+if __name__ == "__main__":
+    # Check number of Arguments, expect 2 (after script itself)
+    # Args: input file, part number
+    if (len(sys.argv) != 3):
+        print(f"Error: Expected 2 arguments, got {len(sys.argv) - 1}")
+
+    else:
+        puzzle(sys.argv[1], sys.argv[2] == '2')
